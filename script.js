8e17c72832fc87ca585ea139877b6bbe924623fd
vahanbarseghyan21 <51117466+vahanbarseghyan21@users.noreply.github.com>
2020-01-13 22:38:31 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,316 @@
+var tableData = [
+    {
+        'name': 'childen',
+        'translation': 'երեխա'
+    },
+    {
+        'name': 'husband', 
+        'translation': 'անուսին'
+    },
+    {
+        'name': 'father',
+        'translation': 'հայր'
+    },
+    {
+        'name': 'mother', 
+        'translation': 'մայր'
+    },
+    {
+        'name': 'parents', 
+        'translation': 'ծնողներ'
+    },
+    {
+        'name': 'daudhter', 
+        'translation': 'աղջիկ'
+    },
+    {
+        'name': 'son', 
+        'translation': 'տղա'
+    },
+    {
+        'name': 'sister', 'translation': 'քույրիկ'},
+    {
+        'name': 'broter', 'translation': 'եղբայր'},
+    {
+        'name': 'grandfather', 'translation': 'պապիկ'},
+    {
+        'name': 'grandmother', 'translation': 'տատիկ'},
+    {
+        'name': 'grandparents', 'translation': 'պապիկ և տատիկ'},
+    {
+        'name': 'granddaughter', 'translation': 'թոռնուհի'},
+    {
+        'name': 'grandson', 'translation': 'թոռնիկ'},
+    {
+        'name': 'uncle', 'translation': 'հորեղբայր և քեռի'},
+    {
+        'name': 'aunts', 'translation': 'մորաքույր և հորաքույր'},
+    {
+        'name': 'niece', 'translation': 'զարմուհի'},
+    {
+        'name': 'nephew', 'translation': 'զարմիկ'},
+    {
+        'name': 'cousins', 'translation': 'զարմիկներ'},
+    {
+        'name': 'weather', 'translation': 'եղանակ'},
+    {
+        'name': 'andry', 'translation': 'զայրացած'},
+    {
+        'name': 'happy', 'translation': 'ուրախ'},
+    {
+        'name': 'hungry', 'translation': 'քաղցած'},
+    {
+        'name': 'sad', 'translation': 'տխուր'},
+    {
+        'name': 'strong', 'translation': 'ուժեղ'},
+    {
+        'name': 'thiruty', 'translation': 'ծարավ'},
+    {
+        'name': 'swimmer', 'translation': 'լողորդ'},
+    {
+        'name': 'height', 'translation': 'բարձրություն'},
+    {
+        'name': 'weight', 'translation': 'քաշ'},
+    {
+        'name': 'short', 'translation': 'կարճահասակ'},
+    {
+        'name': 'tall', 'translation': 'բարձրահասակ'},
+    {
+        'name': 'medium height', 'translation': 'միջին հասակ'},
+    {
+        'name': 'slim ( thin )', 'translation': 'նիհար'},
+    {
+        'name': 'overweight', 'translation': 'չափազանց գեր'},
+    {
+        'name': 'fat', 'translation': 'գեր'},
+    {
+        'name': 'good afternoon', 'translation': 'բարի օր'},
+    {
+        'name': 'good evening', 'translation': 'բարի երեկո'},
+    {
+        'name': 'goodnight', 'translation': 'բարի գիշեր'},
+    {
+        'name': 'sleep well', 'translation': 'լավ քուն'},
+    {
+        'name': 'someone', 'translation': 'ինչ որ մեկը'},
+    {
+        'name': 'quite', 'translation': 'բավականին'},
+    {
+        'name': 'sometimes', 'translation': 'երբեմն'},
+    {
+        'name': 'also', 'translation': 'նաև'},
+    {
+        'name': 'something', 'translation': 'ինչ որ մի բան'},
+    {
+        'name': 'somewhere', 'translation': 'ինչ որ տեղ'},
+    {
+        'name': 'leave', 'translation': 'հեռանալ'},
+    {
+        'name': 'bless you', 'translation': 'առողջություն'},
+    {
+        'name': 'birthday', 'translation': 'տարեդարձ'},
+    {
+        'name': 'difficult', 'translation': 'դժվար'},
+    {
+        'name': 'congratulations', 'translation': 'շնորհավորանքներ'},
+    {
+        'name': 'well done!', 'translation': 'ապրես'},
+    {
+        'name': 'special', 'translation': 'հատուկ'},
+    {
+        'name': 'usually', 'translation': 'սովորաբար'},
+    {
+        'name': 'perhaps', 'translation': 'հավանաբար'},
+    {
+        'name': 'nice to meet you', 'translation': 'ուրախ եմ ձեզ տեսնել'},
+    {
+        'name': 'how are you', 'translation': 'ինչպես եք'},
+    {
+        'name': 'i am fine', 'translation': 'ես լավ եմ'},
+    {
+        'name': 'and you', 'translation': 'իսկ դուք'},
+    {
+        'name': 'poor', 'translation': 'աղքատ'},
+    {
+        'name': 'paupers', 'translation': 'աղքատներ'},
+    {
+        'name': 'for that reason', 'translation': 'այդ պաճառով'},
+    {
+        'name': 'but', 'translation': 'բայց'},
+    {
+        'name': 'to want', 'translation': 'ցանկանալ'},
+    {
+        'name': 'had no beds', 'translation': 'անկողիներ չունեին'},
+    {
+        'name': 'Would you like', 'translation': 'կցանկանակ '},
+    {
+        'name': 'What', 'translation': 'Ինչ'},
+    {
+        'name': 'Who', 'translation': 'Ով'},
+    {
+        'name': 'How', 'translation': 'Ինչպես'},
+    {
+        'name': 'Why', 'translation': 'Ինչու'},
+    {
+        'name': 'how much', 'translation': 'inchqan arje'},
+    {
+        'name': 'toy', 'translation': 'xaxaliq'},
+    {
+        'name': 'this', 'translation': 'ays'},
+    {
+        'name': 'Where', 'translation': 'Որտեղ'},
+    {
+        'name': 'never', 'translation': 'երբեք'},
+    {
+        'name': 'did', 'translation': ' անել'},
+    {
+        'name': 'any', 'translation': 'ցանկացած'},
+    {
+        'name': 'every', 'translation': 'ամեն'},
+    {
+        'name': 'sent', 'translation': 'ուղարկել'},
+    {
+        'name': 'to beg', 'translation': 'խնդրել'},
+    {
+        'name': 'had', 'translation': 'ուներ'},
+    {
+        'name': 'to stand at the side of the road and say', 'translation': 'կանգնել սովորական պողոցում և ասել'},
+    {
+        'name': 'say', 'translation': 'ասել'},
+    {
+        'name': 'road', 'translation': 'ճանապառհ'},
+    {
+        'name': 'penny', 'translation': 'կոպեկ'},
+    {
+        'name': 'bring', 'translation': 'բերել'},
+    {
+        'name': 'evening ', 'translation': 'երեկոյան'},
+    {
+        'name': 'beat them', 'translation': 'հարվածել նրանց'},
+    {
+        'name': 'gave them no food', 'translation': 'չէր տալի նրանց հաց'},
+    {
+        'name': 'feel', 'translation': 'զգալ'},
+    {
+        'name': 'sick', 'translation': 'հիվանդ'},
+    {
+        'name': 'headache ', 'translation': 'գլխացավ'},
+    {
+        'name': 'toothache', 'translation': 'ատամի ցավ'},
+    {
+        'name': 'dentist', 'translation': 'ատամնաբույժ'},
+    {
+        'name': 'hay fever', 'translation': 'խոտի տենդ'},
+    {
+        'name': 'grass', 'translation': 'խոտ'},
+    {
+        'name': 'sneeze', 'translation': 'փռշտոց'},
+    {
+        'name': 'cancer ', 'translation': 'քաղցկեղ'},
+    {
+        'name': 'healthy ', 'translation': 'առողջ'},
+    {
+        'name': 'stressed', 'translation': 'սթրեսի մեջ'},
+    {
+        'name': 'ill', 'translation': 'հիվանդ'},
+    {
+        'name': 'heart', 'translation': 'սիրտ'},
+    {
+        'name': 'heart attack', 'translation': 'սրտի կաթված'}
+]
+
+
+/*
+	1 - Loop Through Array & Access each value
+  2 - Create Table Rows & append to table
+*/
+
+
+var state = {
+    'querySet': tableData,
+    'rows': 15,
+    'window': 5,
+}
+
+buildTable()
+
+function pagination(querySet, page, rows) {
+
+    var trimStart = (page - 1) * rows
+    var trimEnd = trimStart + rows
+
+    var trimmedData = querySet.slice(trimStart, trimEnd)
+
+    var pages = Math.round(querySet.length / rows);
+
+    return {
+        'querySet': trimmedData,
+        'pages': pages,
+    }
+}
+
+function pageButtons(pages) {
+    var wrapper = document.getElementById('pagination-wrapper')
+
+    wrapper.innerHTML = ``
+	console.log('Pages:', pages); 
+
+    var maxLeft = (state.page - Math.floor(state.window / 2))
+    var maxRight = (state.page + Math.floor(state.window / 2))
+
+    if (maxLeft < 1) {
+        maxLeft = 1
+        maxRight = state.window
+    }
+
+    if (maxRight > pages) {
+        maxLeft = pages - (state.window - 1)
+        
+        if (maxLeft < 1){
+        	maxLeft = 1
+        }
+        maxRight = pages
+    }
+    
+    
+
+    for (var page = maxLeft; page <= maxRight; page++) {
+    	wrapper.innerHTML += `<button value=${page} class="page btn btn-sm btn-info success">${page}</button>`;
+    }
+
+    if (state.page != 1) {
+        wrapper.innerHTML = `<button value=${1} class="page btn btn-sm btn-info danger">&#171; First</button>` + wrapper.innerHTML;
+    }
+
+    if (state.page != pages) {
+        wrapper.innerHTML += `<button value=${pages} class="page btn btn-sm btn-info info">Last &#187;</button>`;
+    }
+
+    $('.page').on('click', function() {
+        $('#table-body').empty()
+
+        state.page = Number($(this).val())
+
+        buildTable();
+    })
+
+}
+
+
+function buildTable() {
+    var table = $('#table-body')
+
+    var data = pagination(state.querySet, state.page, state.rows)
+    var myList = data.querySet
+    var num = 1;
+    for (var i = 1 in myList) {
+        //Keep in mind we are using "Template Litterals to create rows"
+        var row = `<tr>
+                  <td>${myList[i].name}</td>
+                  <td>${myList[i].translation}</td>
+                  `
+        table.append(row)
+    }
+    num++;
+    pageButtons(data.pages)
+}
